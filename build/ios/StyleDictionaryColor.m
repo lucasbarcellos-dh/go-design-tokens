6c876cb2068fb0484e479f0c196a53698ab6878b
Automated Publisher <actions@users.noreply.github.com>
2021-03-30 08:52:10 +0000

    Automated publish: Tue Mar 30 08:52:10 UTC 2021 679c925a8a8d889db0d0f5aeda828b8da9a8c4c0

--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Tue, 30 Mar 2021 08:49:58 GMT
+// Generated on Tue, 30 Mar 2021 08:52:09 GMT
 //
 
 #import "StyleDictionaryColor.h"
@@ -21,31 +21,6 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
-rgba(255, 230, 0, 1),
-rgba(112, 141, 242, 1),
-rgba(4, 74, 255, 1),
-rgba(64, 255, 186, 1),
-rgba(64, 255, 186, 1),
-rgba(0, 0, 0, 0.1),
-rgba(4, 74, 255, 1),
-rgba(64, 223, 80, 1),
-rgba(52, 86, 175, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 255, 255, 1),
-rgba(255, 255, 255, 1),
-rgba(207, 48, 48, 1),
-rgba(255, 255, 255, 1),
-rgba(74, 79, 204, 1),
-rgba(255, 255, 255, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 138, 0, 1),
-rgba(255, 46, 0, 1),
-rgba(255, 0, 0, 1),
-rgba(0, 0, 0, 0.25),
-rgba(0, 0, 0, 0.25),
-rgba(0, 0, 0, 1),
-rgba(0, 0, 0, 0.25),
 rgba(14, 25, 26, 1),
 rgba(46, 57, 58, 1),
 rgba(76, 87, 89, 1),
